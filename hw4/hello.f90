6de851374ad7d9367bf33d82cc051c2c71994581
Yanwen Xu <yxu83@grape.soe.ucsc.edu>
2020-05-09 12:39:12 -0700

    hello.f90

--- hw4/hello.f90
+++ hw4/hello.f90
@@ -2,6 +2,14 @@ program main
     use mpi
     implicit none
     
+    integer ierr, myid, numprocs
+
     call MPI_INIT(ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, myid, ierr)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, ierr)
+
+    print *, "Hello world from processor ", myid, " out of ", numprocs
+
+    call MPI_FINALIZE(ierr)
 
 end program main
