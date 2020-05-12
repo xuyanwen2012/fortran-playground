0002b7fe106475e6f0cb46c0fd62a2260a02715d
xuyanwen2012 <xuyanwen2012@gmail.com>
2020-05-12 11:45:14 -0700

    hw5 done

--- hw4/pi.f90
+++ hw4/pi.f90
@@ -18,12 +18,13 @@ program main
     end interface
 
     integer :: ierr, myid, numprocs
+    integer :: stat(MPI_STATUS_SIZE)
     integer :: tag, main_thread, count
     integer :: seed(12)
+    integer :: i, simulation_per_thread, num_in_dart, sum_in_dart
     real :: size
     real :: rnd(1)
     real :: x, y
-    logical :: res
 
     call MPI_INIT(ierr)
     call MPI_COMM_RANK(MPI_COMM_WORLD, myid, ierr)
@@ -32,26 +33,45 @@ program main
     tag = 1234
     main_thread = 0
     count = 1
+    simulation_per_thread = 10000000
+    num_in_dart = 0
+    sum_in_dart = 0
 
     size = 2.0
 
     if (myid == main_thread) then
         ! In main thread we aggreate data from other threads
 
+        do i = 0, numprocs - 1
+            if (i /= myid) then
+                call MPI_RECV(num_in_dart, count, MPI_INTEGER, i, tag, MPI_COMM_WORLD, stat, ierr)
+                sum_in_dart = sum_in_dart + num_in_dart
+                print *, i, ") simulated ", num_in_dart, "/", simulation_per_thread, " darts in circle."
+            endif
+        enddo
+
+        ! Now we can calculate PI
+        print *, "PI = ", (real(sum_in_dart, 8) / (simulation_per_thread * (numprocs - 1))) * 4
+
     else
         ! In other threads we do Monte Carlo simulation to estimate PI
         call RANDOM_SEED(put=seed)
 
-        call RANDOM_NUMBER(rnd)
-        x = rnd(1) * 2 - 1
 
-        call RANDOM_NUMBER(rnd)
-        y = rnd(1) * 2 - 1
+        do i = 1, simulation_per_thread
+            call RANDOM_NUMBER(rnd)
+            x = rnd(1) * 2 - 1
 
-        res = point_in_circle(x, y)
+            call RANDOM_NUMBER(rnd)
+            y = rnd(1) * 2 - 1
 
-        print *, myid, ") ", res
+            if (point_in_circle(x, y)) then
+                num_in_dart = num_in_dart + 1
+            endif
+            
+        enddo
 
+        call MPI_SEND(num_in_dart, count, MPI_INTEGER, main_thread, tag, MPI_COMM_WORLD, ierr)
 
     endif    
 
