a6760834e3ca49d52a99c5dd5a356cf05600aef2
xuyanwen2012 <xuyanwen2012@gmail.com>
2020-05-27 21:04:45 -0700

    matmul

--- hw6/mat_v2.f90
+++ hw6/mat_v2.f90
@@ -19,15 +19,7 @@ program mat_v2
     mat_b = reshape((/ 10, 20, 30, 40, 50, 60, 70, 80, 90 /), shape(mat_a), order = (/2, 1/))
 
     ! Use simple do loop, which iterates each (i,j) cell and compute sum
-    do i = 1, size
-        do j = 1, size
-            sum = 0
-            do k = 1, size
-                sum = sum + mat_a(i, k) * mat_b(k, j)
-            enddo
-            mat_c(i, j) = sum
-        enddo
-    enddo
+    mat_c = matmul(mat_a, mat_b)    
 
     ! Print result
     do i = 1, size
