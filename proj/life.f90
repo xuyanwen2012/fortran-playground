91892b13ee3af2c15f5391fb1ac90edd836c41d6
Yanwen Xu <xuyanwen2012@gmail.com>
2020-06-01 15:57:22 -0700

    refactor

--- proj/life.f90
+++ proj/life.f90
@@ -22,9 +22,10 @@ program main
     logical, dimension(width, height) :: cells = .false.
     logical, dimension(width, height) :: buffer
 
-    integer :: i, j
+    integer :: i, j, k
     integer :: num_live_neighbors
 
+    ! Start MPI
     call MPI_INIT(ierr)
     call MPI_COMM_RANK(MPI_COMM_WORLD, myid, ierr)
     call MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
@@ -45,47 +46,53 @@ program main
         print *, ''
     enddo
 
+    do k = 1, 10
+        ! Clone the state of the universe, do the update on the buffer. 
+        buffer = cells
 
-    buffer = cells
+        ! Tick the universe, 
+        do i= 1, width
+            do j= 1, height
 
-    ! Tick the universe, 
-    do i= 1, width
-        do j= 1, height
+                ! Count number of live neighbors at cell (i, j)
+                if (cells(i, j)) then
+                    num_live_neighbors = sum(count(cells(i-1:i+1, j-1:j+1), 1)) - 1
+                else
+                    num_live_neighbors = sum(count(cells(i-1:i+1, j-1:j+1), 1))
+                endif
 
-            ! Count number of live neighbors at cell (i, j)
-            if (cells(i, j)) then
-                num_live_neighbors = sum(count(cells(i-1:i+1, j-1:j+1), 1)) - 1
-            else
-                num_live_neighbors = sum(count(cells(i-1:i+1, j-1:j+1), 1))
-            endif
+                ! Perform GoF calculation, update the buffer
+                select case (num_live_neighbors)
+                    case (3)
+                        buffer(i, j) = .true.
+                    case (2)
+                        ! Do nothing
+                    case default
+                        buffer(i, j) = .false.
+                endselect
 
-            if (num_live_neighbors .eq. 3) then
-                buffer(i, j) = .true.
-            elseif (num_live_neighbors .eq. 2) then
-                ! Do nothing
-            else
-                buffer(i, j) = .false.
-            endif
-
-            print *, num_live_neighbors
+            enddo
         enddo
-    enddo
 
-    ! Update cells
-    cells = buffer
+        ! Update cells
+        cells = buffer
 
-    ! temp print the universe
-    do i= 1, width
-        do j= 1, height
-            if (cells(i, j)) then
-                write(*, '(A)', advance='no') "X"
-            else
-                write(*, '(A)', advance='no') "O"
-            endif
+        ! temp print the universe
+        do i= 1, width
+            do j= 1, height
+                if (cells(i, j)) then
+                    write(*, '(A)', advance='no') "X"
+                else
+                    write(*, '(A)', advance='no') "O"
+                endif
+            enddo
+            print *, ''
         enddo
         print *, ''
+        
     enddo
 
+    ! End MPI
     call MPI_FINALIZE(ierr)
 
 end program main
