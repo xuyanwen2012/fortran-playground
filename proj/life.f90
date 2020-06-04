2dd85987bf899451fa7bddd37b52e1f38bd4b087
Yanwen Xu <xuyanwen2012@gmail.com>
2020-06-04 11:03:32 -0700

    works on 4x4

--- proj/life.f90
+++ proj/life.f90
@@ -32,6 +32,7 @@ program main
     ! ---------------------------------------------------------------------
 
     ! [height], [width] is the actual board size of this **particular thread**
+    ! TODO: Should be dynamic????
     integer, parameter :: height = 4
     integer, parameter :: width = 4
 
@@ -82,13 +83,20 @@ program main
     left_procs = modulo(my_rank - 1, num_procs)
     right_procs = modulo(my_rank + 1, num_procs)
 
+    ! Debug Print, remove this when finished
+    ! print *, &
+    ! "At rank ", my_rank , &
+    ! "left_procs", left_procs, &
+    ! "right_procs", right_procs, &
+    ! ", sub-width = ", (global_width / num_procs)
+
     ! Initialize main cell 
     if (my_rank .eq. root_rank) then
 
     endif
 
     ! Initialize World  
-    ! Example: A should look like this (4x4)
+    ! Example: [A] should look like this (4x4)
     !
     ! 1  5  9 13
     ! 2  6 10 14
@@ -123,25 +131,56 @@ program main
 
     call MPI_BARRIER(MPI_COMM_WORLD, ierr)
 
+    ! call MPI_Sendrecv( & 
+    !     loc_left, height, MPI_LOGICAL, left_procs, itag, &
+    !     rev_left, height, MPI_LOGICAL, left_procs, itag, &
+    !     MPI_COMM_WORLD, istat, ierr)
+
+    ! call MPI_Sendrecv( & 
+    !     loc_right, height, MPI_INTEGER, right_procs, itag, &
+    !     rev_right, height, MPI_INTEGER, right_procs, itag, &
+    !     MPI_COMM_WORLD, istat, ierr)
+
     call MPI_ISEND(loc_left, height, MPI_INTEGER, left_procs, itag, MPI_COMM_WORLD, irequest, ierr)
     call MPI_ISEND(loc_right, height, MPI_INTEGER, right_procs, itag, MPI_COMM_WORLD, irequest, ierr)
     call MPI_RECV(rev_left, height, MPI_INTEGER, left_procs, itag, MPI_COMM_WORLD, istat, ierr)
     call MPI_RECV(rev_right, height, MPI_INTEGER, right_procs, itag, MPI_COMM_WORLD, istat, ierr)
 
-    ! Prepare augmented cells
-    ! Copy a to d
+    ! Prepare the augmented cells
+
+    ! Copy [A] to [D]
     aug_cells(2 : height + 1, 2 : width + 1) = recv_cells
 
-    ! Copy b, c to d
+    ! At this point, we should have all we need per thread
+    ! Example: [D] should look like this (6x6)
+    ! 
+    !  0  0  0  0  0  0
+    !  0  1  5  9 13  0
+    !  0  2  6 10 14  0
+    !  0  3  7 11 15  0
+    !  0  4  8 12 16  0
+    !  0  0  0  0  0  0
+
+    ! Copy [B], [C] to [D]
     aug_cells(2 : height + 1, 1) = rev_left
     aug_cells(2 : height + 1, width + 2) = rev_right
 
-    ! Copy d's top and bottom
+    ! At this point, we should have all we need per thread
+    ! Example: [D] should look like this (6x6)
+    ! 
+    !  0  0  0  0  0  0
+    ! 21  1  5  9 13 31
+    ! 22  2  6 10 14 32
+    ! 23  3  7 11 15 33
+    ! 24  4  8 12 16 34
+    !  0  0  0  0  0  0
+
+    ! Copy [d]'s top and bottom row
     aug_cells(1, :) = aug_cells(height + 1, :)
     aug_cells(height + 2, :) = aug_cells(2, :)
 
     ! At this point, we should have all we need per thread
-    ! Example: D should look like this (6x6)
+    ! Example: [D] should look like this (6x6)
     ! 
     ! 24  4  8 12 16 34
     ! 21  1  5  9 13 31
@@ -161,79 +200,6 @@ program main
         enddo
         print *, ''
     endif
-
-
-
-    ! ! Compute
-    ! start_col = (width / num_procs) * my_rank + 1
-    ! end_col = start_col + (width / num_procs) - 1
-
-
-    ! ! if (my_rank .eq. 0) then
-    !     ! temp print the universe
-    !     do i = 1, height
-    !         do j = 1, width
-    !             if (recv_buffer(i, j) .ne. 0) then
-    !                 write(*, '(A)', advance='no') "X"
-    !             else
-    !                 write(*, '(A)', advance='no') "O"
-    !             end if
-    !         end do
-    !         print *, ''
-    !     end do
-    !     print *, ''
-    ! ! endif
-
-
-    ! ! Debug Print, remove this when finished
-    ! print *, &
-    ! "At rank ", my_rank , &
-    ! ! "left_procs", left_procs, &
-    ! ! "right_procs", right_procs, &
-    ! ", sub-width = ", (width / num_procs), &
-    ! ", from column ", start_col, &
-    ! " to ", end_col
-
-    ! loc_left = cells(:, start_col)
-    ! loc_right = cells(:, end_col)
-
-    ! ! print *, "loc_left:"
-    ! ! do i = 1, height
-    ! !     print *, loc_left(i)
-    ! ! enddo
-
-    ! ! print *, "loc_right:"
-    ! ! do i = 1, height
-    ! !     print *, loc_right(i)
-    ! ! enddo
-
-    ! call MPI_BARRIER(MPI_COMM_WORLD, ierr)
-
-    ! ! call MPI_Sendrecv( & 
-    ! !     loc_left, height, MPI_LOGICAL, left_procs, itag, &
-    ! !     rev_left, height, MPI_LOGICAL, left_procs, itag, &
-    ! !     MPI_COMM_WORLD, istat, ierr)
-
-    ! ! call MPI_Sendrecv( & 
-    ! !     loc_right, height, MPI_INTEGER, right_procs, itag, &
-    ! !     rev_right, height, MPI_INTEGER, right_procs, itag, &
-    ! !     MPI_COMM_WORLD, istat, ierr)
-
-    ! call MPI_ISEND(loc_left, height, MPI_INTEGER, left_procs, itag, MPI_COMM_WORLD, irequest, ierr)
-    ! call MPI_Isend(loc_right, height, MPI_INTEGER, right_procs, itag, MPI_COMM_WORLD, irequest, ierr)
-    ! call MPI_RECV(rev_left, height, MPI_INTEGER, left_procs, itag, MPI_COMM_WORLD, istat, ierr)
-    ! call MPI_RECV(rev_right, height, MPI_INTEGER, right_procs, itag, MPI_COMM_WORLD, istat, ierr)
-
-
-    ! print *, "rev_left: (", my_rank
-    ! do i = 1, height
-    !     print *, rev_left(i)
-    ! enddo
-
-    ! print *, "rev_right: (", my_rank
-    ! do i = 1, height
-    !     print *, rev_right(i)
-    ! enddo
 
     ! do k = 1, 10
     !     ! Clone the state of the universe, do the update on the buffer. 
