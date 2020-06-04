ad6055dccf5f9ba0dd7ad8d7636d23bfe61202a1
Yanwen Xu <xuyanwen2012@gmail.com>
2020-06-04 09:48:53 -0700

    test matrix documentation

--- proj/test.f90
+++ proj/test.f90
@@ -16,32 +16,46 @@ program p
     integer :: i, j, k
 
     ! Initialize a, b, c  
+    ! Example: A should look like this (4x4)
+    !
+    ! 1  5  9 13
+    ! 2  6 10 14
+    ! 3  7 11 15
+    ! 4  8 12 16
+
     a = reshape((/ (i, i = 1, width * height) /), (/width, height/))
     b = (/ ( 20 + i, i = 1, height) /)
     c = (/ ( 30 + i, i = 1, height) /)
 
     ! Copy a to d
-    do i = 1, height
-        do j = 1, width
-            d(i + 1, j + 1) = a(i, j)
-        enddo
-    enddo
+    d(2 : height + 1, 2 : width + 1) = a
 
     ! Copy b, c to d
-    d(2:height + 1, 1) = b
-    d(2:height + 1, width + 2) = c
+    d(2 : height + 1, 1) = b
+    d(2 : height + 1, width + 2) = c
 
     ! Copy d's top and bottom
     d(1, :) = d(height + 1, :)
     d(height + 2, :) = d(2, :)
 
+    ! At this point, we should have all we need per thread
+    ! Example: D should look like this (6x6)
+    ! 
+    ! 24  4  8 12 16 34
+    ! 21  1  5  9 13 31
+    ! 22  2  6 10 14 32
+    ! 23  3  7 11 15 33
+    ! 24  4  8 12 16 34
+    ! 21  1  5  9 13 31
+
+
     ! Do game of life simulation from (2, 2) to (w+1, h+1)
 
-    do i = 2, height + 1
-        do j = 2, width + 1
-            d(i, j) = d(i, j) + 80
-        enddo
-    enddo
+    ! do i = 2, height + 1
+    !     do j = 2, width + 1
+    !         d(i, j) = d(i, j) + 80
+    !     enddo
+    ! enddo
 
     ! Print the board
     print *, ''
